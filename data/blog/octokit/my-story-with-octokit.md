8c370a622e9a26e7f03032e4f312746bf86e31ce
Oscar Dominguez <dominguez.celada@gmail.com>
2021-05-24 00:47:56 +0200

    feat(blog): add post entries to blog

---
+++ data/blog/octokit/my-story-with-octokit.md
@@ -0,0 +1,10 @@
+---
+title: What is Octoherd?
+date: '2021-05-23'
+tags: ['octokit', 'github-api', 'javascript', 'typescript']
+draft: true
+summary: ''
+images: []
+---
+
+Draft post which should not display
--- data/projectsData.js
+++ data/projectsData.js
@@ -3,13 +3,13 @@ const projectsData = [
     title: 'Octokit',
     description: `Library to extend and simplify how you use the GitHub API.`,
     imgSrc: '/static/images/octokit.png',
-    href: 'https://docs.github.com/en/rest/overview/libraries',
+    href: '/blog/octokit/my-story-with-octokit',
   },
   {
     title: 'Octoherd',
     description: `Manage multiple repository updates all at once.`,
     imgSrc: '/static/images/octoherd.gif',
-    href: 'https://github.com/octoherd',
+    href: '/blog/octoherd/what-is-octoherd',
   },
 ]
 
