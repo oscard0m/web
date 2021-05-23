9af9b80a01324c5e8014b43b8afb4a7d900d397a
Oscar Dominguez <dominguez.celada@gmail.com>
2021-05-23 18:13:24 +0200

    feat(projects): update projects description

--- pages/projects.js
+++ pages/projects.js
@@ -19,7 +19,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Here you can find Open Source projects I\'m collaborating into and Personal Projects
           </p>
         </div>
         <div className="container py-12">
