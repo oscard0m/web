23ab32bb7fe029584abd96076b423f7e3ca31ce9
Oscar Dominguez <dominguez.celada@gmail.com>
2021-05-24 13:12:29 +0200

    feat(now): add Now section

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/now', title: 'Now' },
   { href: '/about', title: 'About' },
 ]
 
