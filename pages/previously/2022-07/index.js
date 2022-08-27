aca0f792e211a4ba81e37120dd8675d5ca11bb26
Oscar Dominguez <dominguez.celada@gmail.com>
2022-08-27 12:42:16 +0200

    fix(previously-july-2022): add month in tittle

--- pages/previously/2022-07/index.js
+++ pages/previously/2022-07/index.js
@@ -13,7 +13,7 @@ export default function Now() {
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Now
+            Now (July 2022)
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
