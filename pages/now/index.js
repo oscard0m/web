3cee98b42cdb5c0ca332df8c2ba291d12504dd41
Oscar Dominguez <dominguez.celada@gmail.com>
2022-09-28 11:17:58 +0200

    fix(now): use <a> when links are external or anchors instead of next/link

--- pages/now/index.js
+++ pages/now/index.js
@@ -20,54 +20,51 @@ export default function Now() {
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
             <p>
               This page was inspired by{' '}
-              <Link href="https://blog.pragmaticengineer.com/now/">Gergely Orosz</Link> and{' '}
-              <Link href="https://sive.rs/nowff">Derek Sivers</Link> now pages
+              <a href="https://blog.pragmaticengineer.com/now/">Gergely Orosz</a> and{' '}
+              <a href="https://sive.rs/nowff">Derek Sivers</a> now pages
               <sup>*</sup>.
             </p>
             <p>Here's what I'm doing now:</p>
             <ul>
               <li>
-                <Link href="#now-im-learning">
+                <a href="#now-im-learning">
                   <span role="img" aria-label="academy">
                     🎓
                   </span>{' '}
                   Now I'm learning
-                </Link>
+                </a>
               </li>
               <li>
-                <Link
-                  href="#open-source-and-personal-pro
-                ects"
-                >
+                <a href="#open-source-and-personal-projects">
                   <span role="img" aria-label="academy">
                     👨🏽‍💻
                   </span>{' '}
                   Open Source and Personal Projects
-                </Link>
+                </a>
               </li>
               <li>
-                <Link href="#now-im-reading">
+                <a href="#now-im-reading">
                   <span role="img" aria-label="books">
                     📚
                   </span>{' '}
                   Now I'm reading
-                </Link>
+                </a>
               </li>
               <li>
-                <Link href="#now-im-watching">
+                <a href="#now-im-watching">
                   <span role="img" aria-label="popcorn">
                     🍿
                   </span>{' '}
                   Now I'm watching
-                </Link>
+                </a>
               </li>
               <li>
-                <Link href="#now-im-playing">
+                <a href="#now-im-playing">
                   <span role="img" aria-label="popcorn">
                     🎮
                   </span>{' '}
                   Now I'm playing
-                </Link>
+                </a>
               </li>
             </ul>
             <p>
@@ -82,11 +79,11 @@ export default function Now() {
             </h1>
             <h2>React</h2>
             <p>
-              In my previous job at <Link href="https://www.marfeel.com/">Marfeel</Link> I did not
-              have the chance to learn and play with <Link href="https://reactjs.org">React</Link>{' '}
-              as much as I would have liked. In my current job at{' '}
-              <Link href="https://www.mainmatter.com">Mainmatter</Link> I'm working on a project
-              built in React.
+              In my previous job at <a href="https://www.marfeel.com/">Marfeel</a> I did not have
+              the chance to learn and play with <a href="https://reactjs.org">React</a> as much as I
+              would have liked. In my current job at{' '}
+              <a href="https://www.mainmatter.com">Mainmatter</a> I'm working on a project built in
+              React.
             </p>
             <p>
               It's good to learn by doing but when we cross that with delivering value in a certain
@@ -94,9 +91,8 @@ export default function Now() {
               decided it's time to start learning the fundamentals and become a React expert.
             </p>
             <p>
-              The course I've chosen to do so is{' '}
-              <Link href="https://epicreact.dev/">Epic React</Link> by{' '}
-              <Link href="https://twitter.com/kentcdodds">Kent C Dodds</Link>.
+              The course I've chosen to do so is <a href="https://epicreact.dev/">Epic React</a> by{' '}
+              <a href="https://twitter.com/kentcdodds">Kent C Dodds</a>.
             </p>
             <p>
               You can read about this journey in my <Link href="/blog">blog</Link>
@@ -104,9 +100,9 @@ export default function Now() {
             <p>
               If you are curious about my progress and submitted solutions to the exercises, you can
               see them{' '}
-              <Link href="https://exercism.org/profiles/dominguezcelada/solutions?track_slug=rust">
+              <a href="https://exercism.org/profiles/dominguezcelada/solutions?track_slug=rust">
                 here
-              </Link>
+              </a>
             </p>
             <br />
             <h1 id="open-source-and-personal-projects">
@@ -117,26 +113,24 @@ export default function Now() {
             </h1>
             <h2>Octokit</h2>
             <p>
-              <Link href="https://github.com/octokit">Octokit</Link> is the official client that can
-              be used to send requests to{' '}
-              <Link href="https://docs.github.com/rest/">GitHub's REST API</Link> and queries to{' '}
-              <Link href="https://docs.github.com/graphql">GitHub's GraphQL API</Link>.
+              <a href="https://github.com/octokit">Octokit</a> is the official client that can be
+              used to send requests to <a href="https://docs.github.com/rest/">GitHub's REST API</a>{' '}
+              and queries to <a href="https://docs.github.com/graphql">GitHub's GraphQL API</a>.
             </p>
             <p>
               Its ecosystem is organized in multiple repositories under{' '}
-              <Link href="https://github.com/octokit">GitHub</Link>, which includes a{' '}
-              <i>plugin system</i> with different <i>plugins</i> you can plug to your Octokit
-              instance.
+              <a href="https://github.com/octokit">GitHub</a>, which includes a <i>plugin system</i>{' '}
+              with different <i>plugins</i> you can plug to your Octokit instance.
             </p>
             <p>
               During this month, I've been working on some maintenance tasks for Octokit
               repositories regarding its CI system. You can find details on my contributions{' '}
-              <Link href="https://github.com/oscard0m?tab=overview">here</Link>.
+              <a href="https://github.com/oscard0m?tab=overview">here</a>.
             </p>
             <h2>What's coming next?</h2>
             <p>
               You can find the next contributions or personal projects I'm planning in my{' '}
-              <Link href="https://github.com/users/oscard0m/projects/1">GitHub Project Board</Link>
+              <a href="https://github.com/users/oscard0m/projects/1">GitHub Project Board</a>
             </p>
             <br />
             <h1 id="now-im-reading">
@@ -151,14 +145,12 @@ export default function Now() {
                 <span role="img" aria-label="book">
                   📚
                 </span>{' '}
-                <Link href="https://www.goodreads.com/book/show/55682280-el-hijo-del-ch-fer">
+                <a href="https://www.goodreads.com/book/show/55682280-el-hijo-del-ch-fer">
                   El hijo del chofer
-                </Link>{' '}
+                </a>{' '}
                 <small>
                   by{' '}
-                  <Link href="https://www.goodreads.com/author/show/4039103.Jordi_Amat">
-                    Jordi Amat
-                  </Link>
+                  <a href="https://www.goodreads.com/author/show/4039103.Jordi_Amat">Jordi Amat</a>
                 </small>
               </li>
             </ul>
@@ -168,18 +160,18 @@ export default function Now() {
                 <span role="img" aria-label="book">
                   📚
                 </span>{' '}
-                <Link href="https://www.goodreads.com/book/show/14744381-the-walking-dead">
+                <a href="https://www.goodreads.com/book/show/14744381-the-walking-dead">
                   The Walking Dead: Compendium Two
-                </Link>{' '}
+                </a>{' '}
                 <small>
                   by{' '}
-                  <Link href="https://www.goodreads.com/author/show/12425.Robert_Kirkman">
+                  <a href="https://www.goodreads.com/author/show/12425.Robert_Kirkman">
                     Robert Kirkman
-                  </Link>{' '}
+                  </a>{' '}
                   and{' '}
-                  <Link href="https://www.goodreads.com/author/show/16962.Charlie_Adlard">
+                  <a href="https://www.goodreads.com/author/show/16962.Charlie_Adlard">
                     Charlie Adlard
-                  </Link>
+                  </a>
                 </small>
               </li>
             </ul>
@@ -189,23 +181,23 @@ export default function Now() {
                 <span role="img" aria-label="book">
                   📚
                 </span>{' '}
-                <Link href="https://www.goodreads.com/book/show/54140556-working-in-public">
+                <a href="https://www.goodreads.com/book/show/54140556-working-in-public">
                   Working in Public: The Making and Maintenance of Open Source Software
-                </Link>{' '}
+                </a>{' '}
                 <small>
-                  by <Link href="https://nadia.xyz/">Nadia Eghbal</Link>
+                  by <a href="https://nadia.xyz/">Nadia Eghbal</a>
                 </small>
               </li>
               <li>
                 <span role="img" aria-label="book">
                   📚
                 </span>{' '}
-                <Link href="https://www.goodreads.com/book/show/59862682-learning-patterns">
+                <a href="https://www.goodreads.com/book/show/59862682-learning-patterns">
                   Learning Patterns
-                </Link>{' '}
+                </a>{' '}
                 <small>
-                  by <Link href="https://www.lydiahallie.io/">Lydia Hallie</Link> and{' '}
-                  <Link href="https://addyosmani.com/">Addy Osmani</Link>
+                  by <a href="https://www.lydiahallie.io/">Lydia Hallie</a> and{' '}
+                  <a href="https://addyosmani.com/">Addy Osmani</a>
                 </small>
               </li>
             </ul>
@@ -222,8 +214,7 @@ export default function Now() {
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film948192.html">The Bear</Link> (Season
-                1) 🇺🇸
+                <a href="https://www.filmaffinity.com/es/film948192.html">The Bear</a> (Season 1) 🇺🇸
               </li>
             </ul>
             <h2>Comedy</h2>
@@ -232,24 +223,22 @@ export default function Now() {
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film393232.html">
-                  Aquí no hay quien viva
-                </Link>{' '}
+                <a href="https://www.filmaffinity.com/es/film393232.html">Aquí no hay quien viva</a>{' '}
                 (Season 2) 🇪🇸
               </li>
               <li>
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film161198.html">Ted Lasso</Link>{' '}
-                (Season 1) 🇬🇧
+                <a href="https://www.filmaffinity.com/es/film161198.html">Ted Lasso</a> (Season 1)
+                🇬🇧
               </li>
               <li>
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film243957.html">White Lotus</Link>{' '}
-                (Season 1) 🇺🇸
+                <a href="https://www.filmaffinity.com/es/film243957.html">White Lotus</a> (Season 1)
+                🇺🇸
               </li>
             </ul>
             <h2>Documentary</h2>
@@ -258,49 +247,45 @@ export default function Now() {
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film537182.html">The Playbook</Link>{' '}
+                <a href="https://www.filmaffinity.com/es/film537182.html">The Playbook</a>{' '}
                 (Mini-series TV) 🇺🇸
               </li>
               <li>
                 <span role="img" aria-label="movie">
                   🎥
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film815791.html">
-                  El silencio de otros
-                </Link>{' '}
+                <a href="https://www.filmaffinity.com/es/film815791.html">El silencio de otros</a>{' '}
                 (2018) 🇪🇸
               </li>
               <li>
                 <span role="img" aria-label="movie">
                   🎥
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film789231.html">Patria</Link> (2019) 🇪🇸
+                <a href="https://www.filmaffinity.com/es/film789231.html">Patria</a> (2019) 🇪🇸
               </li>
               <li>
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film238008.html">
+                <a href="https://www.filmaffinity.com/es/film238008.html">
                   We Need to Talk About Cosby
-                </Link>{' '}
+                </a>{' '}
                 (Mini-series TV) 🇺🇸
               </li>
               <li>
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film253982.html">
-                  Secrets of Playboy
-                </Link>{' '}
+                <a href="https://www.filmaffinity.com/es/film253982.html">Secrets of Playboy</a>{' '}
                 (Mini-series TV) 🇺🇸
               </li>
               <li>
                 <span role="img" aria-label="tv series">
                   📺
                 </span>{' '}
-                <Link href="https://www.filmaffinity.com/es/film452389.html">
+                <a href="https://www.filmaffinity.com/es/film452389.html">
                   Die Wahrheit Uber Franco - Spaniens vergessene Diktatur
-                </Link>{' '}
+                </a>{' '}
                 (Mini-series TV) 🇩🇪
               </li>
             </ul>
@@ -317,9 +302,9 @@ export default function Now() {
                 <span role="img" aria-label="video game">
                   🦊
                 </span>{' '}
-                <Link href="https://www.imdb.com/title/tt12546328/?ref_=nv_sr_srsg_0">
+                <a href="https://www.imdb.com/title/tt12546328/?ref_=nv_sr_srsg_0">
                   Crash Bandicoot 4: It's About Time
-                </Link>{' '}
+                </a>{' '}
                 (2020) 🇺🇸
               </li>
             </ul>
@@ -329,7 +314,7 @@ export default function Now() {
                 <span role="img" aria-label="tv series">
                   ⚽
                 </span>{' '}
-                <Link href="https://www.ign.com/games/fifa-22">Fifa 22</Link> (2021) 🇺🇸
+                <a href="https://www.ign.com/games/fifa-22">Fifa 22</a> (2021) 🇺🇸
               </li>
             </ul>
             <hr></hr>
@@ -337,7 +322,7 @@ export default function Now() {
               <small>
                 <sup>*</sup>
                 Do you want to know more about now pages? Take a look into{' '}
-                <Link href="https://nownownow.com/about">nownownow.com/about</Link>
+                <a href="https://nownownow.com/about">nownownow.com/about</a>
               </small>
             </p>
           </div>
