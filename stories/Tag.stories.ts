573c0359d68311fbb557e61e50b0b3711a59728b
Oscar Dominguez <dominguez.celada@gmail.com>
2023-07-22 19:30:05 +0200

    feat(storybook): add <Tag> to Storybook

---
+++ stories/Tag.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Tag from '../components/Tag'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Tag> = {
+  title: 'Example/Tag',
+  component: Tag,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+}
+
+export default meta
+type Story = StoryObj<typeof Tag>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    text: 'My Awesome Tag',
+  },
+}
