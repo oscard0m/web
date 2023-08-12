eaf08ca13aea0906080abc87faa1e39e53ca06b2
Oscar Dominguez <dominguez.celada@gmail.com>
2023-08-12 18:20:36 +0200

    feat(blog-post): 'How to use 'Nested Layouts' without 'Nested URLs' in Remix'

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -48,7 +48,13 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               <dd>
                 <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
                   <li className="flex items-center space-x-2">
-                    <Img src={siteMetadata.image} alt="avatar" className="w-10 h-10 rounded-full" />
+                    <Img
+                      src={siteMetadata.image}
+                      alt="avatar"
+                      className="w-10 h-10 rounded-full"
+                      width="40"
+                      height="40"
+                    />
                     <dl className="text-sm font-medium leading-5 whitespace-nowrap">
                       <dt className="sr-only">Name</dt>
                       <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
